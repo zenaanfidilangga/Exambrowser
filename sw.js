1f51d8476a2be9973386a7c1c12bbf7278de3d5c
zenaanfidilangga <zenaanfidilangga@gmail.com>
2026-05-04 06:52:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'spesha-exambro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Gunakan cache jika ada (mempercepat load UI)
+        }
+        return fetch(event.request);
+      })
+  );
+});
